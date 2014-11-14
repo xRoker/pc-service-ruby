0465bb66621c890d6bf61e472c5c6c4eab6d12aa
Aliaksei Yeusiukou <xroker@gmail.com>
2014-11-15 01:31:34 +0300

    Added form autocompletion and minor fixes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,11 @@
 class RequestsController < ApplicationController
 
 	def new
-	end
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      redirect_to login_path 
+    end
+  end
 	
 end
