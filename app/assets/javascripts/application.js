6ac57409a66e11fbeb92757aa09ef21367900077
Aliaksei Yeusiukou <xroker@gmail.com>
2014-12-01 01:18:46 +0100

    Moved request js code to the application.js
    Temporary. I don’t know why but it won’t load in any other place.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,4 +62,45 @@ function showPassword() {
     
 }
 
+
+
+function tableClicker() {
+    $('#request-list tr').click(function (event) {
+        var id = $(this).data('value');
+        $('tr').css({"backgroundColor": "#ffffff"});
+        
+        if($("#block"+id).hasClass("out")) {
+            
+            $(".collapse").removeClass("in");
+            $(".collapse").addClass("out");
+            $("#block"+id).addClass("in");
+            $("#block"+id).removeClass("out");
+            $(this).css({"backgroundColor": "#e9ebed"});    
+            $("#block"+id).css({"backgroundColor": "#e9ebed"});
+
+        } else {
+            $("#block"+id).addClass("out");
+            $("#block"+id).removeClass("in");
+        }
+
+    });
+
+    $('#request-tabs li').click(function (event){
+        $('#request-tabs li').removeClass("active");
+        $(this).addClass("active");
+
+        $(".collapse").removeClass("in");
+        $(".collapse").addClass("out");
+
+        if ( $(this).attr('id') == 'pending-tab') {
+            $('.accepted').hide();
+            $('.pending').show();
+        } else {
+            $('.pending').hide();
+            $('.accepted').show();
+        }
+    });
+}
+
+$(document).ready(tableClicker);
 $(document).ready(main);
